6fedac09cd77316b183c7e6df67532201f264375
RayaneKelly <rayanekfreitas@gmail.com>
2022-01-28 14:14:52 -0300

    add Inicial e styles

--- remotus_PlanoB/src/screens/Inicial/index.tsx
+++ remotus_PlanoB/src/screens/Inicial/index.tsx
@@ -14,6 +14,7 @@ import {
   CardButtons,
   CardIcon,
   CardFooter,
+  CardLogo,
   Text,
   TextIcon,
   TextFooter,
@@ -33,11 +34,17 @@ export function Inicial(props) {
   return (
     <Container>
       <Content>
-        <Img xml={ImgInicial} />
-        <Wrapper>
+        <Card>
+          <Img xml={ImgInicial} />
+          <CardLogo>
           <LogoRemotus xml={Logo}
-            opacity={150}
           />
+          </CardLogo>
+        </Card>
+        
+       
+        <Wrapper>
+         
           <WrapperText>
             <Text>Falta pouco para você encontrar um lugar para trabalhar!</Text>
           </WrapperText>
@@ -74,4 +81,81 @@ export function Inicial(props) {
     </Container>
   )
 };
+
+// import React from 'react';
+// import ImgInicial from '../../assets/ImgInicial/ImgInicial';
+// import Logo from '../../assets/Icon/Logo/Logo';
+// import Google from '../../assets/Icon/Google/google';
+// import Facebook from '../../assets/Icon/Facebook/facebook';
+// import Phone from '../../assets/Icon/Phone/phone';
+// import Mail from '../../assets/Icon/Mail/mail';
+
+// import {
+//   Container,
+//   Content,
+//   Wrapper,
+//   Card,
+//   CardButtons,
+//   CardIcon,
+//   CardFooter,
+//   Text,
+//   TextIcon,
+//   TextFooter,
+//   Img,
+//   LogoRemotus,
+//   WrapperText,
+//   WrapperBlock,
+//   IconGoogle,
+//   IconFacebook,
+//   IconPhone,
+//   IconMail,
+
+// } from './styles';
+
+// export function Inicial(props) {
+
+//   return (
+//     <Container>
+//       <Content>
+//         <Img xml={ImgInicial} />
+//         <Wrapper>
+//           <LogoRemotus xml={Logo}
+//             opacity={150}
+//           />
+//           <WrapperText>
+//             <Text>Falta pouco para você encontrar um lugar para trabalhar!</Text>
+//           </WrapperText>
+
+//           <CardButtons>
+//             <IconGoogle xml={Google} />
+//             <TextIcon>Continuar com o Google</TextIcon>
+//           </CardButtons>
+
+//           < CardButtons>
+//             <IconFacebook xml={Facebook} />
+//             <TextIcon>Continuar com o Facebook</TextIcon>
+//           </CardButtons>
+
+//           <WrapperBlock>
+//             <CardIcon>
+//               <IconPhone xml={Phone} />
+//               <TextIcon>Celular</TextIcon>
+//             </CardIcon>
+//             <CardIcon>
+//               <IconMail xml={Mail} />
+//               <TextIcon>Email</TextIcon>
+//             </CardIcon>
+//           </WrapperBlock>
+
+//           <CardFooter>
+//             <TextFooter
+//               onPress={() => props.navigation.navigate('home')}
+
+//             >Continuar como convidado</TextFooter>
+//           </CardFooter>
+//         </Wrapper>
+//       </Content>
+//     </Container>
+//   )
+// };
 
--- remotus_PlanoB/src/screens/Inicial/styles.ts
+++ remotus_PlanoB/src/screens/Inicial/styles.ts
@@ -10,13 +10,13 @@ export const Container = styled.SafeAreaView`
 `;
 export const Content = styled.View`
    align-items:center;
-   justify-content:center;
+   width: 100%;
 `;
 
 export const Wrapper = styled.View`
    align-items:center;
    justify-content:center;
-   margin-Top: -85%;
+   margin-Top: -19%;
    margin-bottom: 60px;
 `;
 export const WrapperBlock = styled.View`
@@ -38,10 +38,15 @@ export const WrapperText = styled.View`
 export const Card = styled.View`
    align-items:center;
    justify-content:center;
-   margin-Top: 10%;
-   
+   margin-Top:10px;
+   width: 100%;
+
+`;export const CardLogo = styled.View`
+      position: absolute;
+      
 
 `;
+
 export const CardButtons = styled.TouchableOpacity`
    flex-direction: row;
    background: ${({ theme }) => theme.colors.shape};
@@ -102,13 +107,16 @@ export const TextFooter = styled.Text`
 `;
 
 export const Img = styled(SvgXml).attrs({
-   width: RFValue(360),
-   height: RFValue(316),
-})``;
+   width: RFValue(500),
+   height: RFValue(320),
+})`
+   
+`;
 
 export const LogoRemotus = styled(SvgXml).attrs({
-   width: RFPercentage(20),
+   width: RFPercentage(25),
    height: RFPercentage(30),
+   
    
 })``;
 
@@ -134,3 +142,143 @@ export const Icon = styled(MaterialIcons)`
   color: ${({ theme }) => theme.colors.warning};
   font-size: ${RFValue(20)}px;
 `;
+
+
+
+
+// import styled from 'styled-components/native';
+// import { SvgXml } from 'react-native-svg';  
+// import { RFValue, RFPercentage } from 'react-native-responsive-fontsize';
+// import { MaterialIcons } from '@expo/vector-icons'; 
+
+// export const Container = styled.SafeAreaView`
+//    flex:1;
+//    background: ${({ theme }) => theme.colors.warning};
+   
+// `;
+// export const Content = styled.View`
+//    align-items:center;
+//    justify-content:center;
+// `;
+
+// export const Wrapper = styled.View`
+//    align-items:center;
+//    justify-content:center;
+//    margin-Top: -85%;
+//    margin-bottom: 60px;
+// `;
+// export const WrapperBlock = styled.View`
+//    flex-direction: row;
+//    justify-content:center;
+//    margin: 10px;
+   
+// `;
+
+// export const WrapperText = styled.View`
+//    width: 80%;
+//    height: 67px;
+//    border-radius: 10px;
+//    background:#b0cb32;
+//    align-items:center;
+//    justify-content:center;
+//    padding: 7px;
+// `;
+// export const Card = styled.View`
+//    align-items:center;
+//    justify-content:center;
+//    margin-Top: 10%;
+   
+
+// `;
+// export const CardButtons = styled.TouchableOpacity`
+//    flex-direction: row;
+//    background: ${({ theme }) => theme.colors.shape};
+//    align-items:center;
+//    justify-content:center;
+//    margin:  20px 0 0 0 ;
+//    width: ${RFValue(300)}px;
+//    height:${RFValue(55)}px;
+//    border-radius: 10px;
+  
+
+// `;
+// export const CardIcon = styled.TouchableOpacity`
+//    flexDirection: row ;
+//    background: ${({ theme }) => theme.colors.shape};
+//    width: ${RFValue(140)}px;
+//    height:${RFValue(55)}px;
+//    border-radius: 10px;
+//    margin:10px;
+//    padding:20px;
+// `;
+// export const CardFooter = styled.TouchableOpacity`
+//    flexDirection: row ;
+//    background: ${({ theme }) => theme.colors.success};
+//    width: ${RFValue(200)}px;
+//    height:${RFValue(40)}px;
+//    border-radius: 10px;
+//    margin:30px 0 0 0;
+//    justify-content: center; 
+// `;
+
+// export const Text = styled.Text`
+//    font-size: ${RFValue(16)}px;
+//    font-family: ${({ theme }) => theme.fonts.bold};
+//    color: ${({ theme }) => theme.colors.shape};
+//    line-height: 19px;
+//    text-align: center;
+//    letter-spacing: 0.15px; 
+    
+// `;
+// export const TextIcon = styled.Text`
+//    font-size: ${RFValue(14)}px;
+//    font-family: ${({ theme }) => theme.fonts.regular};
+//    color: ${({ theme }) => theme.colors.warning};
+//    width: ${RFValue(200)}px;
+//    height:${RFValue(40)}px;
+//    padding: 9px;
+//    border-radius: 10px;
+//    margin-top:-8px;
+  
+// `;
+// export const TextFooter = styled.Text`
+//    font-size: ${RFValue(14)}px;
+//    font-family: ${({ theme }) => theme.fonts.regular};
+//    color: ${({ theme }) => theme.colors.shape};
+//    justify-content:center;
+//    margin-top:12px; 
+// `;
+
+// export const Img = styled(SvgXml).attrs({
+//    width: RFValue(360),
+//    height: RFValue(316),
+// })``;
+
+// export const LogoRemotus = styled(SvgXml).attrs({
+//    width: RFPercentage(20),
+//    height: RFPercentage(30),
+   
+// })``;
+
+// export const IconGoogle = styled(SvgXml).attrs({
+//    width: RFPercentage(3),
+//    height: RFPercentage(3),
+// })``;
+
+// export const IconFacebook = styled(SvgXml).attrs({
+//    width: RFPercentage(3),
+//    height: RFPercentage(3),
+// })``;
+
+// export const IconPhone= styled(SvgXml).attrs({
+//    width: RFPercentage(3),
+//    height: RFPercentage(3),
+// })``;
+// export const IconMail= styled(SvgXml).attrs({
+//    width: RFPercentage(3),
+//    height: RFPercentage(3),
+// })``;
+// export const Icon = styled(MaterialIcons)`
+//   color: ${({ theme }) => theme.colors.warning};
+//   font-size: ${RFValue(20)}px;
+// `;
